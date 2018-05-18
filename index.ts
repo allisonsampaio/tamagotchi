de3b0890ef534ed64ba9f9d300d68c7818e2c7bf
dev3@numerouno.com.br <dev3@numerouno.com.br>
2018-05-17 21:53:41 -0300

    tentando fazer as barras funcionarem e adicao de menu

---
+++ index.ts
@@ -0,0 +1,27 @@
+class Progress {
+	private p: number;
+	private bar = document.querySelectorAll('.status-hunger')[0];
+
+	constructor (p: number) {
+  	this.p = p;
+    this.update();
+  }
+  private update() {
+  	this.bar.style.width = this.p + '%';
+  }  
+  countup() {
+  	if (this.p < 100) { this.p += 10; }
+    this.update();
+  }
+  countdown() {
+  	if (0 < this.p) { this.p -= 10; }
+    this.update();
+  }
+}
+
+var up = document.getElementById('up-hunger');
+var down = document.getElementById('down-hunger');
+var p = new Progress(0);
+
+up.addEventListener('click', () => { p.countup(); });
+down.addEventListener('click', () => { p.countdown(); });
